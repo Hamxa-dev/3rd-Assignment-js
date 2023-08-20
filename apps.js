c7e66dbd98bba58b3446a4923f79a27e0d5b92fd
Hafiz Hamza <hafizhamxa8@gmail.com>
2023-08-20 23:56:19 +0500

    first commit

---
+++ apps.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+// Question = 1
+
+// var paragraph = document.getElementById("myParagraph");
+// paragraph.textContent = "Hello, DOM!";
+
+
+
+
+
+
+
+// Question = 2
+// const myDiv = document.getElementById("myDiv");
+
+// const changeColorButton = document.getElementById("changeColorButton");
+
+// changeColorButton.addEventListener("click", function() {
+
+//   myDiv.style.backgroundColor = "blue";
+// });
+
+
+
+
+
+
+
+
+// Question = 3
+
+// const hiddenTextParagraph = document.getElementById("hiddenText");
+
+// const showTextButton = document.getElementById("showTextButton");
+
+// showTextButton.addEventListener("click", function () {
+//   hiddenTextParagraph.style.display = "block";
+// });
